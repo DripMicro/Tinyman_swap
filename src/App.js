5ff9b3d3ecc6bc5c4bbc78213bd8389f9dd77410
Backendeng <backendeng0417@gmail.com>
2023-01-17 12:18:22 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// routes
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+// components
+import RtlLayout from './components/RtlLayout';
+import ScrollToTop from './components/ScrollToTop';
+import ThemePrimaryColor from './components/ThemePrimaryColor';
+import NotistackProvider from './components/NotistackProvider';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  return (
+    <ThemeConfig>
+      <ThemePrimaryColor>
+        <RtlLayout>
+          <NotistackProvider>
+            <ScrollToTop />
+            <Router />
+          </NotistackProvider>
+        </RtlLayout>
+      </ThemePrimaryColor>
+    </ThemeConfig>
+  );
+}
