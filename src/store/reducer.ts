db8bf42fe110eae49833bf63f9c811a927a2252b
DripMicro <whappydrip@gmail.com>
2023-02-28 23:32:17 +0500

    but fixing Redux

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,3 +1,4 @@
+/* eslint default-case: "error" */
 import * as actionTypes from './actionTypes';
 
 const initialState: ArticleState = {
@@ -17,7 +18,7 @@ const initialState: ArticleState = {
 
 const reducer = (state: ArticleState = initialState, action: ArticleAction): ArticleState => {
   switch (action.type) {
-    case actionTypes.ADD_ARTICLE:
+    case actionTypes.ADD_ARTICLE: {
       const newArticle: IArticle = {
         id: Math.random(), // not really unique but it's just an example
         title: action.article.title,
@@ -27,12 +28,15 @@ const reducer = (state: ArticleState = initialState, action: ArticleAction): Art
         ...state,
         articles: state.articles.concat(newArticle)
       };
-    case actionTypes.REMOVE_ARTICLE:
+    }
+    case actionTypes.REMOVE_ARTICLE: {
       const updatedArticles: IArticle[] = state.articles.filter((article) => article.id !== action.article.id);
       return {
         ...state,
         articles: updatedArticles
       };
+    }
+    // no default
   }
   return state;
 };
